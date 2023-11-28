4f55b32195b8e8ccbbf1e54f1ab17652b0edcf32
TLG <tolga_koyuncu@hotmail.com>
2023-11-28 11:08:58 +0300

    44. ders tamamlandı

--- SignalRApi/Controllers/ContactController.cs
+++ SignalRApi/Controllers/ContactController.cs
@@ -37,14 +37,14 @@ namespace SignalRApi.Controllers
             });
             return Ok("İletişim Bilgisi Eklendi");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteContact(int id)
         {
             var value = _contactService.TGetByID(id);
             _contactService.TDelete(value);
             return Ok("İletişim Bilgisi Silindi");
         }
-        [HttpGet("GetContact")]
+        [HttpGet("{id}")]
         public IActionResult GetCategory(int id)
         {
             var value = _contactService.TGetByID(id);
