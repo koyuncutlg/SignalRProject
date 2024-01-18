fe15fbb5fcbebba884c7e7b580b38dbd98eb3747
TLG <tolga_koyuncu@hotmail.com>
2024-01-18 12:29:10 +0300

    89

---
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutNavBarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutNavBarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
