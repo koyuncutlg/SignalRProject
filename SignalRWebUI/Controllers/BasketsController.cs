82c6a1d5e97a10aeafe1f3b1205d6665f427861b
TLG <tolga_koyuncu@hotmail.com>
2024-01-19 21:43:35 +0300

    :))

--- SignalRWebUI/Controllers/BasketsController.cs
+++ SignalRWebUI/Controllers/BasketsController.cs
@@ -15,7 +15,7 @@ namespace SignalRWebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7027/api/Basket");
+            var responseMessage = await client.GetAsync("https://localhost:7027/api/Basket?id=4");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
