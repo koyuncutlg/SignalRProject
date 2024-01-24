4c4e855d3cbcd983056da988663867fe8efef9ac
TLG <tolga_koyuncu@hotmail.com>
2024-01-24 17:00:03 +0300

    161

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SignalR.EntitiyLayer.Entities;
 using SignalR.EntityLayer.Entities;
 
 namespace SignalR.DataAccessLayer.Concrete
 {
-    public class SignalRContext : DbContext
+    public class SignalRContext : IdentityDbContext<AppUser, AppRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
