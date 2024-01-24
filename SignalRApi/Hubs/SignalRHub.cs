65d70e866fc2e557b16c18cc729f2410e303d97e
TLG <tolga_koyuncu@hotmail.com>
2024-01-24 16:18:23 +0300

    155

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -24,7 +24,9 @@ namespace SignalRApi.Hubs
 			_bookingService = bookingService;
 			_notificationService = notificationService;
 		}
-		public async Task SendStatistic()
+
+		public static int clientCount { get; set; } = 0;
+        public async Task SendStatistic()
 		{
 			var value = _categoryService.TCategoryCount();
 			await Clients.All.SendAsync("ReceiveCategoryCount", value);
@@ -101,5 +103,21 @@ namespace SignalRApi.Hubs
 			var value = _menuTableService.TGetListAll();
 			await Clients.All.SendAsync("ReceiveMenuTableStatus", value);
 		}
-	}
+		public async Task SendMessage(string user, string message)
+		{
+			await Clients.All.SendAsync("ReceiveMessage", user,message);
+		}
+        public override async Task OnConnectedAsync()
+        {
+			clientCount++;
+			await Clients.All.SendAsync("ReceiveClientCount", clientCount);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+			clientCount--;
+			await Clients.All.SendAsync("ReceiveClientCount", clientCount);
+			await base.OnDisconnectedAsync(exception);
+        }
+    }
 }
